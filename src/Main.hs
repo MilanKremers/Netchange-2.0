7b4d2044425c48c28ba191fc19a459d4c90324b5
tjibster <45420236+tjibster@users.noreply.github.com>
2020-01-02 20:08:18 +0100

    ja dingen

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+{-# language NamedFieldPuns #-}
 
 module Main where
 
@@ -10,6 +11,9 @@ import System.Environment
 import System.IO
 import Network.Socket
 
+data Network = Network { u :: Int, n :: Int, dests :: [Int], neighU :: [Int], dU :: [(Int, Int)], nbU :: [(Int, Int)], ndisU :: [(Int, [(Int, Int)])]} deriving (Show)
+data Message = MyDist ((Int, Int)) | Fail (Int) | Repair (Int)
+  
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
@@ -21,7 +25,10 @@ main = do
 
   putStrLn $ "I should be listening on port " ++ show me
   putStrLn $ "My initial neighbours are " ++ show neighbours
- 
+  network <- newIORef (Network { u = me, n = 1, dests = [], neighU = [], dU = [], nbU = [], ndisU = [] })
+  modifyIORef network (updateN (length neighbours))
+  modifyIORef network (updateV (me : neighbours))
+  modifyIORef network (updateNeighU neighbours)
   -- Listen to the specified port.
   serverSocket <- socket AF_INET Stream 0
   setSocketOption serverSocket ReuseAddr 1
@@ -29,9 +36,12 @@ main = do
   listen serverSocket 1024
   -- Let a seperate thread listen for incomming connections
   _ <- forkIO $ listenForConnections serverSocket
-
+  
   -- Initialization of the netchange algorithm
-
+  modifyIORef network (initialize)
+  network'' <- readIORef network
+  putStrLn $ "neighbours " ++ show (network'')
+  
   -- As an example, connect to the first neighbour. This just
   -- serves as an example on using the network functions in Haskell
   case neighbours of
@@ -49,10 +59,47 @@ main = do
       putStrLn "I sent a message to the neighbour"
       message <- hGetLine chandle
       putStrLn $ "Neighbour send a message back: " ++ show message
-      threadDelay 100000000
+      threadDelay 100000
+  
 
-  threadDelay 10000000
+  threadDelay 100000
+-- Initialization function for the IORef network
+initialize :: Network -> Network
+initialize network = network{ ndisU = initNDis (n network) (neighU network) (dests network), 
+                              dU = initDU (u network) (n network) (dests network),
+                              nbU = initNBU (u network) (dests network)}
 
+initNDis :: Int -> [Int] -> [Int] -> [(Int, [(Int, Int)])]
+initNDis _ [] _ = []
+initNDis n (w:ws) vs = singleNeighbourNDis n w vs : initNDis n ws vs
+
+singleNeighbourNDis :: Int -> Int -> [Int] -> (Int, [(Int, Int)])
+singleNeighbourNDis n w vs = (w, singleNeighbourNDis' n vs)
+  where singleNeighbourNDis' :: Int -> [Int] -> [(Int, Int)]
+        singleNeighbourNDis' _ [] = []
+        singleNeighbourNDis' n' (x:xs) = (x, n') : singleNeighbourNDis' n' xs 
+
+initDU :: Int -> Int -> [Int] -> [(Int, Int)] 
+initDU _ _ [] = []
+initDU u n (v:vs) | u == v    = (v, 0) : initDU u n vs
+                  | otherwise = (v, n) : initDU u n vs
+
+initNBU :: Int -> [Int] -> [(Int, Int)] 
+initNBU _  [] = []
+initNBU u (v:vs) | u == v    = (v, 0) : initNBU u vs
+                 | otherwise = (v, -1) : initNBU u vs
+
+-- Update functions for the IORef network
+updateN :: Int -> Network -> Network
+updateN x network = network{ n = n network + x }
+
+updateV :: [Int] -> Network -> Network
+updateV x network = network{ dests = (dests network) ++ x }
+
+updateNeighU :: [Int] -> Network -> Network
+updateNeighU x network = network{ neighU = (neighU network) ++ x }
+
+-- Functions provided in template
 readCommandLineArguments :: IO (Int, [Int])
 readCommandLineArguments = do
   args <- getArgs
